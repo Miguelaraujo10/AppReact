b00f06b14a289c8b2080ff7969874600d4db94d5
Miguel Araujo <miguelaraujo.bar@gmail.com>
2024-07-29 15:04:55 -0300

    Adicionadas novas features usando React Hook

--- src/app/pages/Login/login.tsx
+++ src/app/pages/Login/login.tsx
@@ -1,6 +1,8 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
+   
     const[password,setPassword] = useState('');
     const[email,setEmail] = useState('');
 /*
@@ -24,13 +26,13 @@ export const Login = () => {
 
 
     const emailLength = useMemo(()=>{
-        console.log('Executou')
         return email.length*1000;
     }, [email.length]);
 
     const handleEntrar = useCallback(()=>{
         console.log(email)
         console.log(password)
+
        },[email,password]) 
     
     return (
@@ -40,11 +42,17 @@ export const Login = () => {
 
                 <label >
                     <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)}
+                    onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}
+                    
+                    />
                 </label>
                 <label >
                     <span>Senha</span>
-                    <input  type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                     
+                    <input ref={inputPasswordRef} type="password" value={password} onChange={e => setPassword(e.target.value)}/>
                   
                 </label>
 
